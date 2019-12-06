320b9fdd6e3c4dd365b0acb2e03c178086ed13ce
luizj <luizj@10.104.3.22>
2019-12-06 18:40:05 -0200

    adicionei a pagina home, que contem as listagens dos objetos
    perdidos e o botão para ir pra o login

---
+++ ProjetoPI_ProjetoPI/src/main/java/com/projetopi/controllers/IndexController.java
@@ -0,0 +1,25 @@
+package com.projetopi.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.projetopi.Repository.ObjetoAchadoRepository;
+import com.projetopi.models.ObjetoAchado;
+
+@Controller
+public class IndexController {
+
+	@Autowired
+	public ObjetoAchadoRepository coa;
+	
+	@RequestMapping("/")
+	public ModelAndView listaObjetosAchados() {
+		ModelAndView mv = new ModelAndView("index");
+		Iterable<ObjetoAchado> objetosachados = coa.findAll();
+		mv.addObject("objetosachados", objetosachados);
+		return mv;
+	}
+	
+}
